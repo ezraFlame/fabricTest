2215547a0f81076b2edb6ba2b2a00713d989eab4
ezraFlame <ezrasgamingemail@gmail.com>
2023-04-02 19:06:16 -0400

    Various project setup

--- src/main/java/net/scarzehd/test/mixin/ExampleMixin.java
+++ src/main/java/net/scarzehd/test/mixin/ExampleMixin.java
@@ -1,7 +1,8 @@
-package net.fabricmc.example.mixin;
+package net.scarzehd.test.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.scarzehd.test.ExampleMod;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
